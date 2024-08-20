10da8deb950459502d3fd9eef352399c2433ece9
Erick Gonzales <430328+StriderHND@users.noreply.github.com>
2024-08-20 10:18:04 +0900

    bugfix: unauthorized issues

--- Sources/BSApiClient/BSApiClient.swift
+++ Sources/BSApiClient/BSApiClient.swift
@@ -18,74 +18,64 @@ public class BSApiClient {
         self.waitTime = waitTime
         self.mockMode = isMockMode
     }
-
+    
     public func fetch<T: Codable>(_ request: BSRequestable, session: URLSession = .shared) async throws -> BSResponse<T> {
         guard var urlRequest = mockMode ? request.mockModeURLRequest : request.urlRequst else {
             throw BSNetworkError.invalidRequest
         }
-
+        
         urlRequest.timeoutInterval = TimeInterval(waitTime)
-
-        do {
-            let (data, response) = try await session.data(for: urlRequest)
-
-            guard let httpResponse = response as? HTTPURLResponse else {
-                throw BSNetworkError.invalidResponse
+        
+        
+        let (data, response) = try await session.data(for: urlRequest)
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw BSNetworkError.invalidResponse
+        }
+        
+        let statusCode = httpResponse.statusCode
+        
+        switch statusCode {
+        case 200...299:
+            do {
+                var body: T? = nil
+                
+                if !data.isEmpty {
+                    body = try self.decoder.decode(T.self, from: data)
+                }
+                
+                return BSResponse(code: statusCode, body: body)
+            } catch {
+                throw BSNetworkError.parseError(error: error)
             }
-
-            let statusCode = httpResponse.statusCode
-
-            switch statusCode {
-            case 200...299:
-                do {
-                    var body: T? = nil
-
-                    if !data.isEmpty {
-                        body = try self.decoder.decode(T.self, from: data)
-                    }
-
-                    return BSResponse(code: statusCode, body: body)
-                } catch {
-                    throw BSNetworkError.parseError(error: error)
-                }
-
-            case 300...399:
-                guard let transferError = BSNetworkError.TransferError(rawValue: statusCode) else {
-                    throw BSNetworkError.unknown(message: "\(statusCode)")
-                }
-
-                throw BSNetworkError.transfer(transferError, data: data)
-
-            case 400...499:
-                guard let clientError = BSNetworkError.ClientError(rawValue: statusCode) else {
-                    throw BSNetworkError.unknown(message: "\(statusCode)")
-                }
-
-                throw BSNetworkError.client(clientError, data: data)
-
-            case 500...599:
-                guard let serverError = BSNetworkError.ServerError(rawValue: statusCode) else {
-                    throw BSNetworkError.unknown(message: "\(statusCode)")
-                }
-
-                throw BSNetworkError.server(serverError, data: data)
-
-            default:
+            
+        case 300...399:
+            guard let transferError = BSNetworkError.TransferError(rawValue: statusCode) else {
                 throw BSNetworkError.unknown(message: "\(statusCode)")
             }
-        } catch let error as NSError {
-            switch error.code {
-            case NSURLErrorTimedOut:
-                throw BSNetworkError.client(.requestTimeout, data: nil)
-            case NSURLErrorNotConnectedToInternet, NSURLErrorDataNotAllowed:
-                throw BSNetworkError.collectionLost
-            default:
-                throw BSNetworkError.unknown(message: error.localizedDescription)
+            
+            throw BSNetworkError.transfer(transferError, data: data)
+            
+        case 400...499:
+            guard let clientError = BSNetworkError.ClientError(rawValue: statusCode) else {
+                throw BSNetworkError.unknown(message: "\(statusCode)")
             }
+            
+            throw BSNetworkError.client(clientError, data: data)
+            
+        case 500...599:
+            guard let serverError = BSNetworkError.ServerError(rawValue: statusCode) else {
+                throw BSNetworkError.unknown(message: "\(statusCode)")
+            }
+            
+            throw BSNetworkError.server(serverError, data: data)
+            
+        default:
+            throw BSNetworkError.unknown(message: "\(statusCode)")
         }
     }
-
-
+    
+    
     
     public func getFileSize(fileURL: URL) async throws -> Int {
         try await withCheckedThrowingContinuation { continuation in
@@ -106,7 +96,7 @@ public class BSApiClient {
                     print("[BSApiClient] invalid response.")
                     return continuation.resume(throwing: BSNetworkError.invalidResponse)
                 }
-
+                
                 let contentLength: Int64 = response.expectedContentLength ?? NSURLSessionTransferSizeUnknown
                 return continuation.resume(returning: Int(contentLength))
             }
